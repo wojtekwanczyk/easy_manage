0d856b911fa7a6b946f2cac8820cfc588001a462
Wojtek <wojtekwanczyk@gmail.com>
2019-05-27 17:01:36 +0200

    extend pylint config, fix too long lines and variable naming

--- easy_manage/database.py
+++ easy_manage/database.py
@@ -4,4 +4,4 @@ URI = "mongodb+srv://easy-manage:admin@easy-manage-t2mek.mongodb.net/test?retryW
 
 class DatabaseConnector:
     def __init__(self):
-        self.db = MongoClient(URI).get_default_database()
+        self.database = MongoClient(URI).get_default_database()
