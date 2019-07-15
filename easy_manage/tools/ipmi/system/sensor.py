193945bd22ec1667f148b5cb547c8d4df44ccb15
trejowsk <trejowsk@d17-jagular.ipa.iisg.agh.edu.pl>
2019-07-15 23:51:35 +0200

    Grouped commands in separate files, sensors.py added

---
+++ easy_manage/tools/ipmi/system/sensor.py
@@ -0,0 +1,15 @@
+
+class Sensor:
+    def __init__(self,ipmi):
+        self.ipmi = ipmi
+    
+    #(Set/Get)SensorHysteresis
+    
+    #(Set/Get)SensorThresholds
+    
+    #(Set/Get)SensorEventEnable
+    
+    #GetSensorReading
+    
+    #RearmSensorEvents
+    
