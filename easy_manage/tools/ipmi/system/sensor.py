310dbab31d7b91cdc808221e4fb6b7283b88fd81
trejowsk <trejowsk@d17-jagular.ipa.iisg.agh.edu.pl>
2019-08-15 18:38:47 +0200

    Sensor API simplify

--- easy_manage/tools/ipmi/system/sensor.py
+++ easy_manage/tools/ipmi/system/sensor.py
@@ -7,29 +7,18 @@ class Sensor:
     def __init__(self, ipmi):
         self.ipmi = ipmi
 
-    def read_sensor(self, sensor_number, sdr, lun=0):
-        """Utility used to fetch reading from one specific sensor, with 0 LUN"""
-        raw_reading = self.ipmi.get_sensor_reading(sensor_number, lun)
+    def sensor_reading(self, sdr):
+        """Utility used to fetch reading from one specific sensor, based on it's SDR"""
+        raw_reading = self.ipmi.get_sensor_reading(sdr.number, sdr.lun)
         return sdr.parse_reading(raw_reading)
 
-    def mass_read_sensor(self, sensor_nr_list, sdr_list):
+    def mass_read_sensor(self, sdr_list):
         """
             Mass read for sensors list, assigns to self.reading new reading value
-
-            :param sensor_nr_list: List of sensor numbers
-            :param sdr_list: List of SDR objects
+            :param sdr_list: List of SDR objects whose values need to be fetched
             :return: Dict with <key>:<value> => <sensor_number>:<sensor_reading>
         """
         readings = {}
-        for sensor_number in sensor_nr_list:
-            try:
-                # TODO: Is sensor number sufficient param to identify a sensor?
-                def is_owner_sdr(sdr): return sdr.number == sensor_number
-                sdr = list(filter(is_owner_sdr, sdr_list))[0]
-                readings[sensor_number] = self.read_sensor(
-                    sensor_number, sdr, sdr.lun)
-
-            except IndexError:
-                # TODO: Add logging utility here
-                print("SDR with this number was not found: " + str(sensor_number))
+        for sdr in sdr_list:
+            readings[sdr.name] = self.sensor_reading(sdr)
             return readings
