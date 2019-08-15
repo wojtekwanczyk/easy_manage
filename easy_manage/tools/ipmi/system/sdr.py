d8ca8f455551e2835b639e81c6958fde6bf4aa61
trejowsk <trejowsk@d17-jagular.ipa.iisg.agh.edu.pl>
2019-08-15 18:31:23 +0200

    Moved maps and further implementation of sdrs

--- easy_manage/tools/ipmi/system/sdr.py
+++ easy_manage/tools/ipmi/system/sdr.py
@@ -2,74 +2,17 @@
 from pyipmi.helper import get_sdr_data_helper
 from pyipmi.sdr import SdrCommon
 from pyipmi.errors import DecodingError
-
+from .sdr_maps import SENSOR_TYPE_MAP, RATE_UNIT_MAP, UNIT_MAP, map_code_to_value
 
 SDR_TYPE_FULL_SENSOR_RECORD = 0x01
 SDR_TYPE_COMPACT_SENSOR_RECORD = 0x02
-SENSOR_TYPE_MAP = {
-    0x01: "temperature",
-    0x02: "voltage",
-    0x03: "current",
-    0x04: "fan",
-    0x05: "chassis_intrusion",
-    0x06: "security_violation_attempt",
-    0x07: "processor",
-    0x08: "power_supply",
-    0x09: "power_unit",
-    0xA: "cooling_device",
-    0xB: "other_unit_based_sensor",
-    0xC: "memory",
-    0xD: "drive_slot",
-    0xE: "post_memory",
-    0xF: "firmware_progress",
-    0x10: "evt_logging_disable",
-    0x11: "watchdog_1",
-    0x12: "sys_event",
-    0x13: "crit_interrupt",
-    0x14: "button",
-    0x15: "module_board",
-    0x16: "microcontroller_coprocessor",
-    0x17: "add_in_card",
-    0x18: "chassis",
-    0x19: "chipset",
-    0x1A: "other_fru",
-    0x1B: "cable_interconnect",
-    0x1C: "terminator",
-    0x1D: "system_boot",
-    0x1E: "boot_error",
-    0x1F: "base_os_boot",
-    0x20: "os_stop",
-    0x21: "slot_connector",
-    0x22: "acpi_pwr_state",
-    0x23: "watchdog_2",
-    0x24: "platform_alert",
-    0x25: "entity_presence",
-    0x26: "monitor_asic",
-    0x27: "lan",
-    0x28: "management_sys_health",
-    0x29: "battery",
-    0x2A: "session_audit",
-    0x2B: "ver_change",
-    0x2C: "fru_state"
-}
-RATE_UNIT_MAP = {
-    0b000: "none",
-    0b001: "per uS",
-    0b010: "per ms",
-    0b011: "per s",
-    0b100: "per min",
-    0b101: "per hr",
-    0b110: "per day"
-}
-UNIT_MAP = {
-    # TODO
-
-}
 THRESHOLD_SENSOR_CODE = 0x01
 DISCRETE_SENSOR_RANGE = list(range(0x02, 0x70))
+COMPACT_RECORD_TYPE = 0x02
+FULL_RECORD_TYPE = 0x01
 
 
-def _parse_repository_info(repository):
+def parse_repository_info(repository):
     """ Method parses returned object into a dictionary of entries"""
     return {
         "sdr_version": repository.sdr_version,
@@ -79,44 +22,122 @@ def _parse_repository_info(repository):
     }
 
 
+class AbstractSDR:
+    "Superclass for SDR's, contains common elements"
+
+    def __init__(self, sdr_object):
+        self._sdr_object = sdr_object
+        self.name = sdr_object.device_id_string
+        # Only full and compact SDR are supported
+        self.type = sdr_object.type
+        self.number = sdr_object.number
+        self.lun = sdr_object.owner_lun
+
+    @staticmethod
+    def create_sdr_object(sdr):
+        "Creates SDR object of proper type based on library SDR object"
+        ret_sdr = None
+        if sdr.type == FULL_RECORD_TYPE:
+            ret_sdr = FullSDR(sdr)
+        elif sdr.type == COMPACT_RECORD_TYPE:
+            ret_sdr = CompactSDR(sdr)
+        return ret_sdr
+
+    @staticmethod
+    def get_sensor_kind(reading_code):
+        "Returns sensor class based on given sensor code"
+        if reading_code == THRESHOLD_SENSOR_CODE:
+            return 'threshold'
+        if reading_code in DISCRETE_SENSOR_RANGE:
+            return 'discrete'
+        return 'unsupported'
+
+
+class FullSDR(AbstractSDR):
+    "Class which represents 8 bit sensor with thresholds data record"
+
+    def __init__(self, sdr_object):
+        super().__init__(sdr_object)
+        self.capabilities = sdr_object.capabilities
+
+        self.sensor_type = map_code_to_value(
+            sdr_object.sensor_type_code, SENSOR_TYPE_MAP)
+
+        reading_code = sdr_object.event_reading_type_code
+        self.sensor_kind = AbstractSDR.get_sensor_kind(reading_code)
+
+        self.thresholds = sdr_object.threshold
+        self.unit = {
+            'base_unit': map_code_to_value(sdr_object.units_2, UNIT_MAP),
+            'rate_unit': map_code_to_value(
+                sdr_object.rate_unit, RATE_UNIT_MAP),
+            'percentage': (False, True)[sdr_object.percentage]
+        }
+        self.bounds = {
+            "normal_maximum":  sdr_object.normal_maximum,
+            "normal_minimum":  sdr_object.normal_minimum,
+            "sensor_maximum_reading":  sdr_object.sensor_maximum_reading,
+            "sensor_minimum_reading":  sdr_object.sensor_minimum_reading
+        }
+
+    def parse_reading(self, raw_value):
+        "Parses full SDR sensor reading, provided raw value"
+        return self._sdr_object.convert_sensor_raw_to_value(raw_value)
+
+
+class CompactSDR(AbstractSDR):
+    "Class which represents compact sensor data record"
+
+    def __init__(self, sdr_object):
+        super().__init__(sdr_object)
+        # TODO: Implement
+
+    def parse_reading(self, raw_value):
+        "Parses compact SDR sensor reading, provided raw value"
+        pass
+        # TODO: Implement - provide concatenated reading with value ? sth like dis
+
+
 class SDRRepository:
     """ Class for dealing with retrieving the IPMI SDR Repository data"""
 
     def __init__(self, ipmi):
-        self.ipmi = ipmi
-        self.res_number = None
-        self.repo_info = None
-        self.res_id = None
-        self.sdrs = None
+        self._ipmi = ipmi
+        self._repo_info = None
+        self._sdrs = None
 
-    def fetch_sdr_repository(self):
-        """ Returns current SDR repository data"""
+    def fetch_sdr_object_list(self):
+        """ Returns list of SDR objects"""
 
-        repo = self.ipmi.get_sdr_repository_info()
-        repo = _parse_repository_info(repo)
-        if self._is_stale(repo) or self.sdrs is None:
+        repo = self._ipmi.get_sdr_repository_info()
+        repo = parse_repository_info(repo)
+        if self._is_stale(repo) or self._sdrs is None:
             sdrs = self._fetch_sdrs()
         else:
-            sdrs = self.sdrs
-        # TODO: Mapping SDRS into objects
+            sdrs = self._sdrs
+        unfiltered = list(map(AbstractSDR.create_sdr_object, sdrs))
+        def isNone(x): return x is not None
+        filtered = list(filter(isNone, unfiltered))
+        self._sdrs = filtered
+        return filtered
 
     def fetch_repository_info(self):
         """ Fetches and saves the data in the object"""
-        if not self.repo_info:
-            repo = self.ipmi.get_sdr_repository_info()
-            self.repo_info = _parse_repository_info(repo)
-        return self.repo_info
+        if not self._repo_info:
+            repo = self._ipmi.get_sdr_repository_info()
+            self._repo_info = parse_repository_info(repo)
+        return self._repo_info
 
     def _fetch_sdrs(self):
         """ Fetches SDR entries from the BMC"""
         tmp_sdrs = self._get_repository_sdr_list()
-        self.sdrs = list(filter(lambda x: (x is not None), tmp_sdrs))
-        return self.sdrs
+        self._sdrs = list(filter(lambda x: (x is not None), tmp_sdrs))
+        return self._sdrs
 
     def _get_repository_sdr(self, record_id, reservation_id=None):
         """ Method for fetching a single record, and next record's ID in a tuple"""
         (next_id, record_data) = get_sdr_data_helper(
-            self.ipmi.reserve_sdr_repository, self.ipmi._get_sdr_chunk,
+            self._ipmi.reserve_sdr_repository, self._ipmi._get_sdr_chunk,
             record_id, reservation_id)
         try:
             return (SdrCommon.from_data(record_data, next_id), next_id)
@@ -127,7 +148,7 @@ class SDRRepository:
 
     def _sdr_repository_entries(self):
         """ Generator of tuples (next_id, entry)"""
-        reservation_id = self.ipmi.reserve_sdr_repository()
+        reservation_id = self._ipmi.reserve_sdr_repository()
         record_id = 0
 
         while True:
@@ -144,68 +165,7 @@ class SDRRepository:
 
     def _is_stale(self, new_info):
         """ Does object needs to re-fetch data from BMC"""
-        curr = self.repo_info
+        curr = self._repo_info
         if curr is None or curr["most_recent_addition"] != new_info["most_recent_addition"]:
             return True
         return False
-
-
-class AbstactSDR:
-    "Superclass for SDR's, contains common elements"
-
-    def __init__(self, sdr_object):
-        self.sdr_object = sdr_object
-        self.name = sdr_object.device_id_string
-        # Only full and compact SDR are supported
-        self.type = sdr_object.type
-
-
-class FullSDR(AbstactSDR):
-    "Class which represents 8 bit sensor with thresholds data record"
-
-    def __init__(self, sdr_object):
-        super().__init__(sdr_object)
-        # TODO: Decide if this is needed at all
-        self.capabilities = sdr_object.capabilities
-
-        self.sensor_type = self._map_code_to_value(
-            sdr_object.sensor_type_code, SENSOR_TYPE_MAP)
-
-        reading_code = sdr_object.event_reading_type_code
-        self.sensor_kind = self._get_sensor_kind(reading_code)
-
-        self.thresholds = sdr_object.threshold
-        # TODO: Reimplement Full Record class fucking hell GOD DAMMIT FUUUUUUUUUCK, and then base unit should work
-
-        self.unit = {
-            'base_unit': self._map_code_to_value(sdr_object.units_2, UNIT_MAP),
-            'rate_unit': self._map_code_to_value(
-                sdr_object.rate_unit, RATE_UNIT_MAP),
-            'percentage': (False, True)[sdr_object.percentage]
-        }
-        self.bounds = {
-            "normal_maximum":  sdr_object.normal_maximum,
-            "normal_minimum":  sdr_object.normal_minimum,
-            "sensor_maximum_reading":  sdr_object.sensor_maximum_reading,
-            "sensor_minimum_reading":  sdr_object.sensor_minimum_reading
-        }
-
-    def _map_code_to_value(self, type_code, type_map):
-        try:
-            return type_map[type_code]
-        except KeyError:
-            return 'unsupported'
-
-    def _get_sensor_kind(self, reading_code):
-        if(reading_code == THRESHOLD_SENSOR_CODE):
-            return 'threshold'
-        elif reading_code in DISCRETE_SENSOR_RANGE:
-            return 'discrete'
-
-
-class CompactSDR(AbstactSDR):
-    "Class which represents compact sensor data record"
-
-    def __init__(self, sdr_object):
-        super().__init__(sdr_object)
-        # TODO: Implement
--- easy_manage/tools/ipmi/system/sensor.py
+++ easy_manage/tools/ipmi/system/sensor.py
@@ -7,18 +7,29 @@ class Sensor:
     def __init__(self, ipmi):
         self.ipmi = ipmi
 
-    def read_sensor(self, sensor_number, lun=0):
+    def read_sensor(self, sensor_number, sdr, lun=0):
         """Utility used to fetch reading from one specific sensor, with 0 LUN"""
-        return self.ipmi.get_sensor_reading(sensor_number, lun)
+        raw_reading = self.ipmi.get_sensor_reading(sensor_number, lun)
+        return sdr.parse_reading(raw_reading)
 
-    def mass_read_sensor(self, sensors_list):
+    def mass_read_sensor(self, sensor_nr_list, sdr_list):
         """
             Mass read for sensors list, assigns to self.reading new reading value
-            TODO: Perhaps create own data structure for storing sensor objects
 
-            :param: List of sensor objects
+            :param sensor_nr_list: List of sensor numbers
+            :param sdr_list: List of SDR objects
             :return: Dict with <key>:<value> => <sensor_number>:<sensor_reading>
         """
         readings = {}
-        for sensor_number in sensors_list:
-            readings[sensor_number] = self.read_sensor(sensor_number)
+        for sensor_number in sensor_nr_list:
+            try:
+                # TODO: Is sensor number sufficient param to identify a sensor?
+                def is_owner_sdr(sdr): return sdr.number == sensor_number
+                sdr = list(filter(is_owner_sdr, sdr_list))[0]
+                readings[sensor_number] = self.read_sensor(
+                    sensor_number, sdr, sdr.lun)
+
+            except IndexError:
+                # TODO: Add logging utility here
+                print("SDR with this number was not found: " + str(sensor_number))
+            return readings
