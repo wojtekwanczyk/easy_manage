febc218fdd247c2bf5377920105c35c85b8c280c
Wojtek <wojtekwanczyk@gmail.com>
2019-08-28 21:57:10 +0200

    add bash memory methods

--- easy_manage/shells/bash_shell.py
+++ easy_manage/shells/bash_shell.py
@@ -11,6 +11,8 @@ class BashShell:
         self.credentials = credentials
         self.client = None
 
+    # Base commands
+
     def connect(self):
         self.client = SSHClient()
         self.client.set_missing_host_key_policy(AutoAddPolicy())
@@ -22,7 +24,32 @@ class BashShell:
     def disconnect(self):
         self.client.close()
 
-    def cmd(self, cmd):
+    def execute(self, cmd):
         stdin, stdout, stderr = self.client.exec_command(cmd)
-        for line in stdout:
-            print('... ' + line.strip('\n'))
+        return list(stdout)
+
+    # Memory commands
+
+    def get_memory_total(self, swap=False):
+        "Return total device memory in kibibytes"
+        row = '2'
+        if swap:
+            row = '3'
+        cmd = f"free -m | awk 'NR=={row}{{printf $2}}'"
+        output = self.execute(cmd)
+        return int(output[0])
+
+    def get_memory_used(self, swap=False):
+        "Return device memory in use in kibibytes"
+        row = '2'
+        if swap:
+            row = '3'
+        cmd = f"free -m | awk 'NR=={row}{{printf $3}}'"
+        output = self.execute(cmd)
+        return int(output[0])
+
+    def get_memory_percentage(self, swap=False):
+        "Return device memory usage in percentage"
+        total = self.get_memory_total(swap)
+        used = self.get_memory_used(swap)
+        return float(used/total)
