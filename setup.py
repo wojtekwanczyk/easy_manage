4c73e4e9c76ec35bd395ea241f6a142b4e9a8389
trejowsk <trejowsk@d17-jagular.ipa.iisg.agh.edu.pl>
2019-09-08 15:05:17 +0200

    interlock

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r") as f:
 REQUIREMENTS_LIST = [
     'redfish >= 2.1.0',
     'pymongo[srv] >= 3.8.0',
-    'python-ipmi >= 0.4.1',
-    'cryptography >= 2.7'
+    'cryptography >= 2.7',
+    'python-ipmi'
 ]
 
 setuptools.setup(
@@ -21,7 +21,7 @@ setuptools.setup(
     url="https://github.com/wojtekwanczyk/easy_manage",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
@@ -29,5 +29,8 @@ setuptools.setup(
         'console_scripts': [
             'easy_manage = easy_manage.__init__:main']
     },
-    install_requires=REQUIREMENTS_LIST
+    install_requires=REQUIREMENTS_LIST,
+    dependency_links=[
+        'http://github.com/Arcticae/python-ipmi/tarball/master#egg=python-ipmi-0'
+    ]
 )
