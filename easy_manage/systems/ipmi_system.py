4be9fbc3c203c2f1259f4597a5c93d13c43818bc
sborkows <borkowskiszymon28@gmail.com>
2019-10-29 22:54:32 +0100

    Deleted not working ipmi chunks

--- easy_manage/systems/ipmi_system.py
+++ easy_manage/systems/ipmi_system.py
@@ -2,17 +2,12 @@
 import logging
 
 from easy_manage.systems.abstract_system import AbstractSystem
-from easy_manage.tools.ipmi.system.fru import FRU
-from easy_manage.tools.ipmi.system.info import Info
-from easy_manage.tools.ipmi.system.sdr import SDR
-from easy_manage.tools.ipmi.system.sel import SEL
-from easy_manage.tools.ipmi.system.sensor import Sensor
 
 LOGGER = logging.getLogger('ipmi_system')
 LOGGER.setLevel(logging.DEBUG)
 
 
-class IpmiSystem(AbstractSystem, FRU, SEL, SDR, Info, Sensor):
+class IpmiSystem(AbstractSystem):
     """
         Class meant for aggregating all of the sub-functionalities listed:
         FRU - Field Replaceable Unit
