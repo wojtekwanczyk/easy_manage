796a4b2ed803d84fb5e143a95dd30f4c80cc1b6e
Tomasz Rejowski <tomasz.rejowski@swmansion.com>
2019-11-20 02:45:50 +0100

    Naming fix vol2

--- easy_manage/tools/ipmi/reducers/ipmi_reducer.py
+++ easy_manage/tools/ipmi/reducers/ipmi_reducer.py
@@ -50,7 +50,7 @@ class IpmiReducer:
         return {
             'properties': SystemReducer.StaticsReducer.basic_info(self.sys_buffer, self.chass_buffer),
             'memory_size_gb': SystemReducer.StaticsReducer.mem_size_gb(self.sys_buffer, self.chass_buffer),
-            'cpus': SystemReducer.StaticsReducer.cpus_info(self.sys_buffer, self.chass_buffer),
+            'cpu_info': SystemReducer.StaticsReducer.cpus_info(self.sys_buffer, self.chass_buffer),
             'storage': SystemReducer.StaticsReducer.storage_info(self.sys_buffer, self.chass_buffer),
             'pci_e': SystemReducer.StaticsReducer.pci_info(self.sys_buffer, self.chass_buffer),
             # 'sensors': SystemReducer.StaticsReducer.sensors_info(self.sys_buffer, self.chass_buffer) #TODO: Implement
